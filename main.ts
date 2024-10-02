7daa931bdbbeb0b472fb0847b90008d87c820c00
Dammo447420 <62548209+Dammo44@users.noreply.github.com>
2024-10-02 18:36:17 +0200

    funk gruppe + logo hinzufügung

--- main.ts
+++ main.ts
@@ -1,39 +1,91 @@
-let Funk = 0
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    if (0 == _1) {
+        basic.showNumber(receivedNumber)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    Funk += 1
-    basic.showNumber(Funk)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    if (0 == _1) {
+        Funk += 1
+        basic.showNumber(Funk)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    radio.sendNumber(Funk)
+    if (0 == _1) {
+        radio.sendNumber(Funk)
+    }
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    Funk += -1
-    basic.showNumber(Funk)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    if (_1 == 0) {
+        Funk += -1
+        basic.showNumber(Funk)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
+let Funk = 0
+let _1 = 0
+_1 = 1
+radio.setGroup(44)
+basic.showLeds(`
+    # . # . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # . # . .
+    # # # . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # . # . .
+    # # # . .
+    . . # . #
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # . # . .
+    # # # . .
+    . . # . #
+    . . # # #
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # . # . .
+    # # # . .
+    . . # . #
+    . . # # #
+    . . . . #
+    `)
+basic.showString("System start")
+_1 = 0
